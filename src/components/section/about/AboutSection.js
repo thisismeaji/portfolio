a76f9dbdae389dec72856532de0a7de91f56c75f
thisismeaji <thisismeaji20@gmail.com>
2025-01-05 11:12:36 +0700

    add about head section

--- src/components/section/about/AboutSection.js
+++ src/components/section/about/AboutSection.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Styles from "../about/about.module.css";
+import Image from 'next/image';
 
 export default function AboutSection  () {
   return (
     <div className={Styles.aboutSection}>
-      
+      <div className={Styles.aboutImage}>
+        <Image
+          src="/images/profile.png"
+          width={1000}
+          height={1000}
+          alt='Profile aji'
+        />
+      </div>
+      <div className={Styles.aboutText}>
+        <h1>About Me</h1>
+        <h2>Hello There, I’m Ajisaka Kamandanu.</h2>
+        <p>I graduated with a Bachelor's degree in Information Technology from Universitas Muhammadiyah Purworejo with a GPA of 3.2. As a fresh graduate, I have a strong passion for frontend development and a high level of enthusiasm to make a positive impact in this industry.</p>
+        <p>I enjoy exploring new ideas, solving problems, and continuously developing my skills to stay relevant in the ever-evolving world of frontend development.</p>
+        <div className={Styles.aboutCTA}>
+          <button>Hire Me</button>
+          <button>Donwload CV</button>
+        </div>
+      </div>
     </div>
   );
 }
