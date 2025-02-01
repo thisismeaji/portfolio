477b0f8d8090f498611dc457cf89221ccea8b213
thisismeaji <thisismeaji20@gmail.com>
2025-02-01 19:33:38 +0700

    remove background hero except home

--- src/components/section/about/AboutSection.js
+++ src/components/section/about/AboutSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Styles from "../about/about.module.css";
 import Image from 'next/image';
-import HeroButton from '@/components/button/herobutton/HeroButton';
+import BodyButton from '@/components/button/bodybutton/BodyButton';
 
 export default function AboutSection  () {
   return (
@@ -21,7 +21,7 @@ export default function AboutSection  () {
         <p>As a fresh graduate, I possess a strong passion for frontend development, adaptability, and a great willingness to learn new technologies in the field of web development.</p>
         <p>I am enthusiastic about taking on challenges in creating user-friendly and responsive interfaces, enjoy collaborating within development teams, and am determined to make meaningful contributions through innovative and efficient solutions.</p>
         <p>I am committed to continuous growth as a frontend developer and ready to seize opportunities that allow me to enhance my skills while adding value to the projects I work on.</p>
-        <HeroButton text="My Portfolio" link="/portfolio"/>
+        <BodyButton text="My Portfolio" link="/portfolio"/>
       </div>
     </section>
   );
